316c1aa232e80be478dd53f227a124f300eb67d1
HangwelaniN <hngwekhulu@gmail.com>
2023-11-19 19:48:44 +0200

    added html and css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,39 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <div className="main">
+        <div className="display ">00</div>
+
+        <div className="calculator">
+          <input type="Button" className="color" value="AC" />
+          <input type="Button" className="color" value="DE" />
+          <input type="Button" className="color" value="." />
+          <input type="Button" className="color" value="/" />
+        </div>
+        <div className="calculator">
+          <input type="Button" value="7" />
+          <input type="Button" value="8" />
+          <input type="Button" value="9" />
+          <input type="Button" className="color" value="*" />
+        </div>
+        <div className="calculator">
+          <input type="Button" value="4" />
+          <input type="Button" value="5" />
+          <input type="Button" value="6" />
+          <input type="Button" className="color" value="-" />
+        </div>
+        <div className="calculator">
+          <input type="Button" value="1" />
+          <input type="Button" value="2" />
+          <input type="Button" value="3" />
+          <input type="Button" className="color" value="+" />
+        </div>
+        <div className="calculator">
+          <input type="Button" value="0" />
+          <input type="Button" value="00" />
+          <input type="Button" className="equal" value="=" />
+        </div>
+      </div>
     </div>
   );
 }
